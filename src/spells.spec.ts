163e5859cf1d1e36074f9079d0c9cabae016002b
Sidoine De Wispelaere <sidoine@sidoine.net>
2020-12-04 15:57:01 +0100

    Update spells

--- src/spells.spec.ts
+++ src/spells.spec.ts
@@ -2,6 +2,5 @@ import test from "ava";
 import { SpellId } from "./spells";
 
 test("spells", (t) => {
-    // t.is(SpellId.volatile_bomb, 271050);
-    t.is(SpellId.volatile_bomb, 271045);
+    t.is(SpellId.agony, 980);
 });
