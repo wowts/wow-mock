6eeef8a315b262026f792ccecc9df288c0bb6767
Sidoine De Wispelaere <sidoine@sidoine.net>
2020-12-20 11:35:29 +0100

    fix: update spells

--- src/spells.spec.ts
+++ src/spells.spec.ts
@@ -3,4 +3,7 @@ import { SpellId } from "./spells";
 
 test("spells", (t) => {
     t.is(SpellId.agony, 980);
+    t.is(SpellId.judgment, 20271);
+    t.is(SpellId.judgment_holy, 275773);
+    t.is(SpellId.judgment_protection, 275779);
 });
